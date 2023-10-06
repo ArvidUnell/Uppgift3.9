65b7af7aa02f7e906aed87fd51b0ce425c62c375
arun06001 <arun06001@E-PW0234KE>
2023-10-06 11:05:48 +0200

    Add project files.

---
+++ Uppgift3.9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Uppgift3_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en addition med 1 eller 2 plustecken");
+            string problem = Console.ReadLine();
+
+            short plusIndex1 = (short)problem.IndexOf('+');
+            short plusIndex2 = (short)problem.IndexOf('+',plusIndex1+1);
+
+            float tal1 = float.Parse(problem[..plusIndex1]);
+            float tal2;
+            float tal3 = 0;
+
+            if (plusIndex2 == -1)
+            {
+                tal2 = float.Parse(problem[(plusIndex1+1)..]);
+            }
+            else
+            {
+                tal2 = float.Parse(problem[(plusIndex1+1)..plusIndex2]);
+                tal3 = float.Parse(problem[(plusIndex2 + 1)..]);
+            }
+
+            Console.WriteLine($"={tal1 + tal2 + tal3}");
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
